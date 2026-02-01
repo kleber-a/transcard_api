34fae621eeb5271fa23bbff70d3c22f92b694617
Kleber <kleberandrade1998@hotmail.com>
2026-02-01 10:44:38 -0300

    Removendo comentários

--- src/main/java/com/kleber/transcard/dto/request/CreateCardDTO.java
+++ src/main/java/com/kleber/transcard/dto/request/CreateCardDTO.java
@@ -12,33 +12,3 @@ public record CreateCardDTO(
         CardType cardType
 ) {}
 
-//public class CreateCardDTO {
-//    private Long cardNumber;
-//    private String cardName;
-//    private CardType cardType;
-//
-//
-//    public Long getCardNumber() {
-//        return cardNumber;
-//    }
-//
-//    public void setCardNumber(Long cardNumber) {
-//        this.cardNumber = cardNumber;
-//    }
-//
-//    public String getCardName() {
-//        return cardName;
-//    }
-//
-//    public void setCardName(String cardName) {
-//        this.cardName = cardName;
-//    }
-//
-//    public CardType getCardType() {
-//        return cardType;
-//    }
-//
-//    public void setCardType(CardType cardType) {
-//        this.cardType = cardType;
-//    }
-//}
